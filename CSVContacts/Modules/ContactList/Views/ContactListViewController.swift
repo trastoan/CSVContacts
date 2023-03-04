06a5f323d6b7b4598479bf1fbaad787bf475dc71
Trastoan <yurisaboiaf@gmail.com>
2023-03-04 12:21:01 -0300

    adds root router

---
+++ CSVContacts/Modules/ContactList/Views/ContactListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContactListViewController.swift
+//  CSVContacts
+//
+//  Created by Yuri on 04/03/23.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
