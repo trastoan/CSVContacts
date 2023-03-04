b2641b0c26e1f32ee30ecac726edeb57df5991a0
Trastoan <yurisaboiaf@gmail.com>
2023-03-04 09:49:16 -0300

    initial commit

---
+++ CSVContacts/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  CSVContacts
+//
+//  Created by Yuri on 04/03/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
