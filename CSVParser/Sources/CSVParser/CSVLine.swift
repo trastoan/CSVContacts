d91b1d107df88ac5430381fdb00e42601c214384
Trastoan <yurisaboiaf@gmail.com>
2023-03-04 09:52:06 -0300

    creates package for parser

---
+++ CSVParser/Sources/CSVParser/CSVLine.swift
@@ -0,0 +1,13 @@
+//
+//  CSVLine.swift
+//  
+//
+//  Created by Yuri on 04/03/23.
+//
+
+import Foundation
+
+public struct CSVLine {
+    public var lineNumber: Int
+    public var data: [String?]
+}
