5e24a4214556ad36a0b2a35296be190ab4bee69f
Trastoan <yurisaboiaf@gmail.com>
2023-03-05 23:55:28 -0300

    added load from local contacts

--- CSVContacts/Services/ContactService.swift
+++ CSVContacts/Services/ContactService.swift
@@ -21,24 +21,46 @@ enum ContactServiceErrors: Error {
 struct ContactService: ContactServiceProtocol {
     private let contactFileName: String
     private let fileExtension: String
+    private let savedContactsDirectory: URL
 
-    init(fileName: String = "sample_contacts", fileExtension: String = "csv") {
+    init(fileName: String = "sample_contacts", fileExtension: String = "csv", directory: URL? = nil) {
         self.contactFileName = fileName
         self.fileExtension = fileExtension
+
+        if let directory {
+            self.savedContactsDirectory = directory
+        } else {
+            self.savedContactsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        }
     }
 
     func getContacts() -> AnyPublisher<[Contact], Error> {
-        guard let url = Bundle.main.url(forResource: contactFileName, withExtension: fileExtension) else {
-            return Fail(error: ContactServiceErrors.resourceNotFound).eraseToAnyPublisher()
-        }
-        let parser = CSVParser(url: url, lineBreaker: "\r\n")
+        var contacts = [Contact]()
 
-        guard let contacts = try? parser.decode(type: Contact.self) else {
-            return Fail(error: ContactServiceErrors.decodeError).eraseToAnyPublisher()
+        if let savedContacts = retrieveEditedContacts() {
+            contacts = savedContacts
+        } else {
+            contacts = retrievePreloadedContacts() ?? []
         }
         
         return Just(contacts)
             .setFailureType(to: Error.self)
             .eraseToAnyPublisher()
+    }
+
+    private func retrieveEditedContacts() -> [Contact]? {
+        let url = savedContactsDirectory.appendingPathExtension(contactFileName)
+        guard let data = try? Data(contentsOf: url) else { return nil }
+
+        return try? JSONDecoder().decode([Contact].self, from: data)
+    }
+
+    private func retrievePreloadedContacts() -> [Contact]? {
+        guard let url = Bundle.main.url(forResource: contactFileName, withExtension: fileExtension) else {
+            return nil
+        }
+
+        let parser = CSVParser(url: url, lineBreaker: "\r\n")
+        return try? parser.decode(type: Contact.self)
     }
 }
